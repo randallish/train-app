661e766fac20f1ca775eb9c08b70fc809bd7ae96
randall ishino <rishino24@gmail.com>
2018-01-19 15:05:09 -0800

    libraries added

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAR6zZxo6orXiylc0-YEWv4MMK4CBUKyG4",
+    authDomain: "train-app-82493.firebaseapp.com",
+    databaseURL: "https://train-app-82493.firebaseio.com",
+    projectId: "train-app-82493",
+    storageBucket: "train-app-82493.appspot.com",
+    messagingSenderId: "750269279062"
+  };
+  firebase.initializeApp(config);
